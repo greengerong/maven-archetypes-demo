a1ec9cac82010ccf73167fbbc3a7980d0633d1aa
greengerongg <grzx2210@163.com>
2014-03-12 21:48:07 +1100

    Initial commit

---
+++ src/main/resources/archetype-resources/src/test/java/ComponentTest.java
@@ -0,0 +1,10 @@
+package $package;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+
+public class TestProject {
+
+}
---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,10 @@
+package $package;
+
+/**
+ * this is a demo for maven archetypes plugins.
+ */
+public class TestProject {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
