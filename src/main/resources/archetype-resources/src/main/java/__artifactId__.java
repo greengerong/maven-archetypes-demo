96ec66973ebe8466fcce0e305138fd6a68c3df88
greengerongg <grzx2210@163.com>
2014-03-17 20:40:37 +1100

    demo for chage file name to artifactId

---
+++ src/main/resources/archetype-resources/src/main/java/__artifactId__.java
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+mvn archetype:generate -DarchetypeGroupId=com.github.greengerong -DarchetypeArtifactId=component -DarchetypeVersion=0.0.1-SNAPSHOT -DgroupId=com.github.greengerong -DartifactId=demo
+</body>
+</html>
