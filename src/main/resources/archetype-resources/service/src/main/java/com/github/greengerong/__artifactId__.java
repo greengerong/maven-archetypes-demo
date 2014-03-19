7b072c8d6dd67b50a9a0597f195cd975b4ea0ccc
greengerongg <grzx2210@163.com>
2014-03-19 22:12:18 +1100

    demo for chage file name to artifactId

--- src/main/resources/archetype-resources/service/src/main/java/com/github/greengerong/__artifactId__.java
+++ src/main/resources/archetype-resources/service/src/main/java/com/github/greengerong/__artifactId__.java
@@ -1,10 +1,10 @@
-package $package;
+package com.github.greengerong;
 
 /**
  * this is a demo for maven archetypes plugins.
  */
-public class TestProject {
+public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("${artifactId}");
     }
 }
--- src/main/resources/archetype-resources/src/main/java/__artifactId__.java
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-</head>
-<body>
-mvn archetype:generate -DarchetypeGroupId=com.github.greengerong -DarchetypeArtifactId=component -DarchetypeVersion=0.0.1-SNAPSHOT -DgroupId=com.github.greengerong -DartifactId=demo
-</body>
-</html>
